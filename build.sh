82f5d8ae9921ebe4532e38ed9824c6f732c99fb6
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-04-04 18:21:17 -0400

    Updated ISO Publisher.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e -u
 
 iso_name=archlinux
 iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <http://www.archlinux.org>"
+iso_publisher="Tryton Van Meer <https://www.trytonvanmeer.dev>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version=$(date +%Y.%m.%d)
 install_dir=arch
